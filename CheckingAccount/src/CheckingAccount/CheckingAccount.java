f736e46ee298e3c2e324e2bd09f2f642291b6c96
hyojung <hyojung@192.168.173.180>
2017-03-25 20:04:33 +0900

    hi

--- CheckingAccount/src/CheckingAccount/CheckingAccount.java
+++ CheckingAccount/src/CheckingAccount/CheckingAccount.java
@@ -5,5 +5,16 @@ public class CheckingAccount extends Account {
 	private int interest;
 	private int loan_interest;
 	@Override
+	public int debit(int b){
+		balance() -=b;
+		if(balance<0){
+			System.out.printf("Not Enough Money!\n");
+			balance +=b;
+			return 0;
+		}
+		else
+			return balance;
+		
+	}
 	
-}
+};
